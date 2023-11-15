34cbb0854739724f44d5c71cebe6a78669782003
Dmytro Vukolov <dmytro.vukolov@Dmytros-MacBook-Pro.local>
2023-11-15 14:21:42 +0100

    dll test

--- test_dll.py
+++ test_dll.py
@@ -2,14 +2,14 @@ from ctypes import *
 
 dll_name = "./cmasterrace.so"
 input_message = "Hello world!"
-result = "f"
-result_pointer = c_wchar_p(result)
+result = 1
+result_pointer = c_void_p(result)
 
-#print(result_pointer)
+# print(result_pointer)
 
 libc = CDLL(dll_name)
 encoded_message = libc.base64_encode(
-    c_char_p(input_message),
+    c_wchar_p(input_message),
     c_int(len(input_message)),
     result_pointer)
 libc.base64_cleanup()
