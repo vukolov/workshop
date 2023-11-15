ff46187f52aaab333fe0ff82cc1479e06ec596e0
Dmytro Vukolov <dmytro.vukolov@Dmytros-MacBook-Pro.local>
2023-11-15 10:30:26 +0100

    first commit

---
+++ server.py
@@ -0,0 +1,13 @@
+import asyncio
+from websockets.server import serve
+
+async def echo(websocket):
+    async for message in websocket:
+        message = 'bla-bla-' + message
+        await websocket.send(message)
+
+async def main():
+    async with serve(echo, "localhost", 42069):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
